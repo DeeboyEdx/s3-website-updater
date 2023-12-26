6ee6c07aacf09cd52c6b8e5fb7878927e14838bd
DeeboyEdx <71462840+DeeboyEdx@users.noreply.github.com>
2023-12-26 16:37:34 -0500

    Remove file from version control

--- sync_pcpusher_website.bat
+++
@@ -1,3 +0,0 @@
-echo Note: This was designed to be run on my personal computer
-pause
-pwsh -noprofile -command "deactivate; .\.win-venv\Scripts\activate; .\s3-uploader.ps1 -ProjectRoot C:\Users\aquar\OneDrive\Documents\QuikScripts\python\push2pc\s3-html\ -BucketName pccommander.net -DistributionId ETFTX83TEXR9E -SyncJustChanges"
